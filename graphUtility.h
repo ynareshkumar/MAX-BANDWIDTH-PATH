4aca9ecdbdf0b7d3cc4005b4c10ad114cc28cf0f
ynareshkumar <nareshonline90@gmail.com>
2014-03-12 18:52:59 -0500

    Update graphUtility.h

--- graphUtility.h
+++ graphUtility.h
@@ -3,12 +3,12 @@
 
 #include <vector>
 #define NO_OF_VERTICES 5000
-#define VERTEX_DEGREE 6
+#define VERTEX_DEGREE 1000
 
 using namespace std;
 
 static std::vector<int> adjmatrix(NO_OF_VERTICES*NO_OF_VERTICES);
-static unsigned int vertexdegree[NO_OF_VERTICES];
+extern unsigned int vertexdegree[NO_OF_VERTICES];
 
 struct edgedetails{
 	unsigned int edgeweight;
@@ -20,8 +20,8 @@ class GraphInterface
 {
 public:
   //GraphInterface();
-  //virtual ~MyObjectInterface() {};
-  virtual unsigned int addedge(int srcvertex,int dstvertex,unsigned int edgeweight) = 0;
+  //virtual ~MyObjectIn
+  virtual void addedge(int srcvertex,int dstvertex,unsigned int edgeweight) = 0;
   virtual void display(int srcvertex) = 0;
   virtual void initialize() = 0;
 };
