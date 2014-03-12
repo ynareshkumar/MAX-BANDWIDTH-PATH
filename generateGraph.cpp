faf7be503e8cdbed02d099dfde5ece752cf1f02d
ynareshkumar <nareshonline90@gmail.com>
2014-03-12 18:55:29 -0500

    Update generateGraph.cpp

--- generateGraph.cpp
+++ generateGraph.cpp
@@ -11,30 +11,42 @@ int main()
 	int dstvertex = -1, edgewt; 
 	unsigned int deg=0;
 	int maxvertex, minvertex=0;
-	int maxedge = 15000, minedge = 1;
+	int maxedge = 15000,minedge = 1;
 	maxvertex = NO_OF_VERTICES-1;
 	GraphInterface *graph;
 	graph = createInstance();
 	graph->initialize();
+	int ctr;
+	
 	for(int i=0;i < NO_OF_VERTICES ;i++)
-	{	deg=0;	
+	{	//deg=0;	
 		//minvertex = i;
 		//cout<<"\nRandom dst vertex for src vertex "<<i<<" is ";
-		while(deg < VERTEX_DEGREE)
+		ctr=0;
+		while(vertexdegree[i] < VERTEX_DEGREE)
 		{
+			
+			//ctr++;
+			//cout<<"\n ctr is "<<ctr;
+			if(ctr >  NO_OF_VERTICES)
+				break;
 			do
 			{
+				ctr++;
 				dstvertex = rand()%(maxvertex-minvertex+1)+minvertex; // Generate the number, assign to variable.
-				//cout<<dstvertex<<" ";
-			}while(dstvertex == i);
+				//cout<<vertexdegree[dstvertex]<<" ";
+				if(ctr > NO_OF_VERTICES)
+					break;
+			}while(dstvertex == i || vertexdegree[dstvertex] >= VERTEX_DEGREE);
 			edgewt = rand()%(maxedge-minedge+1)+minedge;
-			deg = graph->addedge(i,dstvertex,edgewt);		
-			//cout<<"\n gen graph degree "<<deg<<endl;		
+			graph->addedge(i,dstvertex,edgewt);		
+			//cout<<"\n gen graph degree for src vertex "<<i<<" is "<<vertexdegree[i]<<endl;		
 		}
 	}
+	//cout<<"Printing..\n";
 	for(int k=0;k < NO_OF_VERTICES ;k++)
 	{	
 		graph->display(k);
-	}	
+	}
 	return 0;
-}
+	}
