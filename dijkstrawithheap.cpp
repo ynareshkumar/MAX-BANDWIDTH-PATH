2910e6ecd6273f088567889f7d9b32ab90a09af0
ynareshkumar <nareshonline90@gmail.com>
2014-03-19 20:01:34 -0500

    Create dijkstrawithheap.cpp

---
+++ dijkstrawithheap.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "maxheap.h"
+
+int main()
+{
+	/*maxheapinterface *heap;
+	heap = createheapInstance();
+	
+	heap->heapinitialize();
+	
+	heap->insert(4);		
+	heap->insert(9);
+	heap->insert(6);		
+	heap->insert(45);
+	heap->insert(89);
+	heap->insert(386);
+	heap->insert(20);
+
+	heap->remove(0);
+	heap->displayheap();
+	
+	heap->updatevalue(0,100);
+	heap->displayheap();
+	heap->updatevalue(1,101);
+	heap->displayheap();
+	heap->updatevalue(1,8);
+	heap->displayheap();
+	heap->updatevalue(0,1);
+	heap->displayheap();*/
+	
+	return 0;
+}
