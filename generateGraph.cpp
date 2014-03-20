7f103955fe07e06176f1f4c51d72b3c8b2f03cac
ynareshkumar <nareshonline90@gmail.com>
2014-03-20 17:31:50 -0500

    Update generateGraph.cpp

--- generateGraph.cpp
+++ generateGraph.cpp
@@ -5,48 +5,79 @@
 
 using namespace std;
 
-int main()
+class creategraph : public gengraphinterface
 {	
-	srand(time(NULL)); // Seed the time
-	int dstvertex = -1, edgewt; 
-	unsigned int deg=0;
-	int maxvertex, minvertex=0;
-	int maxedge = 15000,minedge = 1;
-	maxvertex = NO_OF_VERTICES-1;
-	GraphInterface *graph;
-	graph = createInstance();
-	graph->initialize();
-	int ctr;
+	public:
 	
-	for(int i=0;i < NO_OF_VERTICES ;i++)
-	{	//deg=0;	
-		//minvertex = i;
-		//cout<<"\nRandom dst vertex for src vertex "<<i<<" is ";
-		ctr=0;
-		while(vertexdegree[i] < VERTEX_DEGREE)
-		{
-			
-			//ctr++;
-			//cout<<"\n ctr is "<<ctr;
-			if(ctr >  NO_OF_VERTICES)
-				break;
-			do
+	int dstvertex, edgewt; 
+	int ctr,breakaway;
+	unsigned int deg;
+	int maxvertex, minvertex;
+	int maxedge,minedge;
+	
+	creategraph()
+	{
+		
+	}
+	
+	void generaterandomgraph()
+	{
+		dstvertex = -1;
+		deg=0;		
+		minvertex=0;
+		maxedge = 15000;
+		minedge = 1;
+	
+		srand(time(NULL)); // Seed the time
+		
+		maxvertex = NO_OF_VERTICES-1;
+		GraphInterface *graph;
+		graph = creategraphInstance();
+		graph->initialize();
+	
+	
+		for(int i=0;i < NO_OF_VERTICES ;i++)
+		{	//deg=0;	
+			//minvertex = i;
+			//cout<<"\nRandom dst vertex for src vertex "<<i<<" is ";
+			ctr=0;
+			breakaway = 1;
+			while(vertexdegree[i] < VERTEX_DEGREE)
 			{
-				ctr++;
-				dstvertex = rand()%(maxvertex-minvertex+1)+minvertex; // Generate the number, assign to variable.
-				//cout<<vertexdegree[dstvertex]<<" ";
-				if(ctr > NO_OF_VERTICES)
+	
+				//ctr++;
+				//cout<<"\n ctr is "<<ctr;
+				if(ctr >  NO_OF_VERTICES || !breakaway)
 					break;
-			}while(dstvertex == i || vertexdegree[dstvertex] >= VERTEX_DEGREE);
-			edgewt = rand()%(maxedge-minedge+1)+minedge;
-			graph->addedge(i,dstvertex,edgewt);		
-			//cout<<"\n gen graph degree for src vertex "<<i<<" is "<<vertexdegree[i]<<endl;		
+				do
+				{
+					ctr++;
+					dstvertex = rand()%(maxvertex-minvertex+1)+minvertex; // Generate the number, assign to variable.
+					//cout<<vertexdegree[dstvertex]<<" ";
+					if(ctr > NO_OF_VERTICES)
+					{
+						breakaway=0;
+						break;
+					}
+				}while(dstvertex == i || vertexdegree[dstvertex] >= VERTEX_DEGREE);
+				if(breakaway)
+				{
+					edgewt = rand()%(maxedge-minedge+1)+minedge;
+					graph->addedge(i,dstvertex,edgewt);	
+				}	
+				//cout<<"\n gen graph degree for src vertex "<<i<<" is "<<vertexdegree[i]<<endl;		
+			}
 		}
+		//cout<<"Printing..\n";
+		/*for(int k=0;k < NO_OF_VERTICES ;k++)
+		{	
+			graph->display(k);
+		}	*/
+			
 	}
-	//cout<<"Printing..\n";
-	for(int k=0;k < NO_OF_VERTICES ;k++)
-	{	
-		graph->display(k);
-	}
-	return 0;
-	}
+};
+
+gengraphinterface * createrandomgraphInstance()
+		{
+			return new creategraph();
+		}
