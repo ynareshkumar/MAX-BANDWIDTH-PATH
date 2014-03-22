2900a5e7253dafd187ffa7b58f836ea9f5c0e164
ynareshkumar <nareshonline90@gmail.com>
2014-03-22 17:06:11 -0500

    Update dijkstrawithheap.cpp

--- dijkstrawithheap.cpp
+++ dijkstrawithheap.cpp
@@ -1,14 +1,82 @@
 #include <iostream>
+#include <limits.h>
 #include "maxheap.h"
+#include "graphUtility.h"
+
+int s =2,t = 4;
+int dad[NO_OF_VERTICES];
+
+int minimum(int a,int b)
+{
+	return a < b ? a:b;
+}
 
 int main()
 {
-	/*maxheapinterface *heap;
-	heap = createheapInstance();
+	gengraphinterface * randomgraph;
+	int u, v = 0,noofedges = 0;
+	int distu,distv;
 	
+	randomgraph = createrandomgraphInstance();
+	randomgraph->generaterandomgraph();		
+	
+	cout<<"\nGraph successfully generated"<<endl;
+	
+	maxheapinterface *heap;
+	heap = createheapInstance();
+	dad[s] = -1;
 	heap->heapinitialize();
 	
-	heap->insert(4);		
+	heap->updatevalue(s,INT_MAX);
+		
+	
+	while(heapsize > 0)
+	{
+		u = heap->remove(0);
+		distu = heap->getvalue(u);
+		//cout<<"u is "<<u<<" dist of u "<<distu<<endl;
+		noofedges = 0;
+		v = 0;
+		while(noofedges < VERTEX_DEGREE && v < NO_OF_VERTICES)
+		{
+			while(adjmatrix[u*NO_OF_VERTICES+v] == 0)
+			{
+				v++;
+			}
+			//cout<<"V selected is "<<v<<endl;
+			if(v < NO_OF_VERTICES)
+			{
+				noofedges++;
+				distv = heap->getvalue(v);
+				//cout<<"Min value chosen is "<<minimum(distu,adjmatrix[u*NO_OF_VERTICES+v])<<endl;
+				if(distv < minimum(distu,adjmatrix[u*NO_OF_VERTICES+v]))
+				{
+					distv = minimum(distu,adjmatrix[u*NO_OF_VERTICES+v]);
+					dad[v] = u;
+					heap->updatevalue(v,distv);
+				}
+				
+				v++;
+			}
+												
+		}
+	}
+	int dst = t;	
+	cout<<"Max bandwidth for vertex "<<t<<" from "<<s<<" is "<<D[t]<<endl;
+	
+	cout<<"The path between "<<dst<<" and "<<s<<" is "<<endl;
+	cout<<dst;
+	while(dst != s)
+	{
+		cout<<"->"<<dad[dst];
+		dst = dad[dst];
+	}
+	cout<<endl;
+			
+	return 0;
+}
+	
+	/*heap->insert(4);		
 	heap->insert(9);
 	heap->insert(6);		
 	heap->insert(45);
@@ -27,6 +95,4 @@ int main()
 	heap->displayheap();
 	heap->updatevalue(0,1);
 	heap->displayheap();*/
-	
-	return 0;
-}
+
