2b2a586b8e55bfd5796680f24c3c259e3fe7b751
ynareshkumar <nareshonline90@gmail.com>
2014-03-22 17:08:21 -0500

    Update maxheap.h

--- maxheap.h
+++ maxheap.h
@@ -1,12 +1,19 @@
 #ifndef MAXHEAPINTERFACE_H
 #define MAXHEAPINTERFACE_H
 
+#include "graphUtility.h"
+
+extern int H[NO_OF_VERTICES];
+extern int heapsize;
+extern int D[NO_OF_VERTICES];
+
 class maxheapinterface
 {
-	public:
+	public:		
 	virtual void heapinitialize() = 0;
 	virtual void insert(int val) = 0;
-	virtual void remove(int index) = 0;
+	virtual int remove(int index) = 0;
+	virtual int getvalue(int i) = 0;
 	virtual void displayheap() = 0;
 	virtual void updatevalue(int heapindex,int val) = 0;
 };
