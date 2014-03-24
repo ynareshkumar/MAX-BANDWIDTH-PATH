58b6b2163cd5971134d52caeb1d40ea7e044df02
ynareshkumar <nareshonline90@gmail.com>
2014-03-23 20:25:55 -0500

    Update maxheap.h

--- maxheap.h
+++ maxheap.h
@@ -5,7 +5,17 @@
 
 extern int H[NO_OF_VERTICES];
 extern int heapsize;
+extern int edgesize;
 extern int D[NO_OF_VERTICES];
+
+struct edges
+{
+	int u;
+	int v;
+	int edgeweight;
+};
+
+extern std::vector<struct edges> edgeparameters;
 
 class maxheapinterface
 {
@@ -16,6 +26,8 @@ class maxheapinterface
 	virtual int getvalue(int i) = 0;
 	virtual void displayheap() = 0;
 	virtual void updatevalue(int heapindex,int val) = 0;
+	virtual void heapsort() = 0;
+	virtual struct edges extractmax() = 0;
 };
 
 
