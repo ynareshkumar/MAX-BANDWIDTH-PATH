6510acf37326f8acd78eca604164a732bd715834
ynareshkumar <nareshonline90@gmail.com>
2014-04-05 23:31:37 -0500

    Update kruskal.cpp

--- kruskal.cpp
+++ kruskal.cpp
@@ -2,93 +2,113 @@
 #include <stack> 
 #include "maxheap.h"
 #include "graphUtility.h"
-
-gengraphinterface * randomgraph;
-maxheapinterface *heap;
-int dad[NO_OF_VERTICES];
-int rank[NO_OF_VERTICES];
+#include "kruskal.h"
 
 std::vector<int> maxspanningtree(NO_OF_VERTICES*NO_OF_VERTICES);
 
-void makeset(int v)
+class kruskalmstimplementation:public maxspanningtreeinterface
 {
-	dad[v] = 0;
-	rank[v] = 0;
-}
-
-int find(int v)
-{
-	std::stack<int> S;
-	int w = v, u;
+	private:
+	maxheapinterface *heap;
+	int dad[NO_OF_VERTICES];
+	int rank[NO_OF_VERTICES];
 	
-	while(dad[w] != 0)
+	void makeset(int v)
 	{
-		S.push(w);
-		w = dad[w];
+		dad[v] = -1;
+		rank[v] = 0;
 	}
 	
-	while(!S.empty())
+	int find(int v)
 	{
-		u = S.top();
-		S.pop();
-		dad[u] = w;
-	}
-	
-	return w;
-	
-}
-
-void merge(int r1,int r2)
-{
-	if(rank[r1] < rank[r2])
-		dad[r1] = r2;
-	else if(rank[r1] > rank[r2])
-		dad[r2] = r1;
-	else
-	{
-		dad[r2] = r1;
-		rank[r1] += 1;
-	}
-}
-
-void kruskalalgorithm()
-{
-	struct edges e;
-	int r1,r2,u;
-	heap->heapsort();
-	
-	for(int i=0;i<NO_OF_VERTICES;i++)
-		makeset(i);
-	
-	while(edgesize > 0)
-	{
-		e = heap->extractmax();
-		//cout<<" u is "<<e.u<<" v is "<<e.v<<" wt is "<<e.edgeweight<<endl;
-		r1 = find(e.u);
-		r2 = find(e.v);
-		if(r1 != r2)
+		std::stack<int> S;
+		int w = v, u;
+		
+		while(dad[w] != -1)
 		{
-			maxspanningtree[(e.u)*NO_OF_VERTICES+(e.v)] = e.edgeweight;
-			merge(r1,r2);
+			S.push(w);
+			w = dad[w];
 		}
 		
-	} 
-}
-
-void displaymst(int srcvertex)
-{
-	cout<<"Source vertex "<<srcvertex<<endl;
-	cout<<"Dst vertex Edge weight"<<endl;
-	for(int i=0;i<NO_OF_VERTICES;i++)
-	{
-		if (maxspanningtree[srcvertex*NO_OF_VERTICES+i] != 0)
+		while(!S.empty())
 		{
-			cout<<i<<"\t    "<<maxspanningtree[srcvertex*NO_OF_VERTICES+i]<<endl;			
+			u = S.top();
+			S.pop();
+			dad[u] = w;
+		}
+		//cout<<"w for "<<v<<" is "<<w<<endl;
+		return w;
+		
+	}
+	
+	void merge(int r1,int r2)
+	{	
+		if(rank[r1] < rank[r2])
+			dad[r1] = r2;
+		else if(rank[r1] > rank[r2])
+			dad[r2] = r1;
+		else
+		{
+			dad[r2] = r1;
+			rank[r1] += 1;
+		}
+			//cout<<"rank[r1] "<<rank[r1]<<" rank[r2] "<<rank[r2]<<endl;
+	}
+	
+	public:
+	
+	kruskalmstimplementation()
+	{
+		heap = createheapInstance();
+	}
+	
+	void kruskalalgorithm()
+	{
+		struct edges e;
+		int r1,r2,u;		
+		//heap->heapsort();
+		int edgesadded = 0;
+		for(int i=0;i<NO_OF_VERTICES;i++)
+			makeset(i);
+		
+		while(edgesize > 0 && edgesadded < NO_OF_VERTICES-1)
+		{
+			e = heap->extractmax();			
+			r1 = find(e.u);
+			r2 = find(e.v);
+			//cout<<" u value is "<<e.u<<" v is "<<e.v<<" wt is "<<e.edgeweight<<" r1 is "<<r1<<" r2 is "<<r2<<endl;
+			if(r1 != r2 && edgesadded < NO_OF_VERTICES-1)
+			{
+				edgesadded++;
+				//cout<<" u is "<<e.u<<" v is "<<e.v<<" wt is "<<e.edgeweight<<" r1 is "<<r1<<" r2 is "<<r2<<endl;
+				maxspanningtree[(e.u)*NO_OF_VERTICES+(e.v)] = e.edgeweight;
+				maxspanningtree[(e.v)*NO_OF_VERTICES+(e.u)] = e.edgeweight;
+				merge(r1,r2);
+			}
+			
+		} 
+	}
+	
+	void displaymst(int srcvertex)
+	{
+		cout<<"Source vertex "<<srcvertex<<endl;
+		cout<<"Dst vertex Edge weight"<<endl;
+		for(int i=0;i<NO_OF_VERTICES;i++)
+		{
+			if (maxspanningtree[srcvertex*NO_OF_VERTICES+i] != 0)
+			{
+				cout<<i<<"\t    "<<maxspanningtree[srcvertex*NO_OF_VERTICES+i]<<endl;			
+			}
 		}
 	}
-}
+};
 
-int main()
+maxspanningtreeinterface * createmstInstance()
+		{
+			return new kruskalmstimplementation();
+		}
+
+/*int main()
 {
 	
 	randomgraph = createrandomgraphInstance();
@@ -96,7 +116,7 @@ int main()
 	
 	cout<<"\nGraph successfully generated"<<endl;
 		
-	heap = createheapInstance();	
+		
 	kruskalalgorithm();
 	for(int k=0;k < NO_OF_VERTICES ;k++)
 	{
@@ -104,4 +124,4 @@ int main()
 	}
 	
 	return 0;
-}
+}*/
