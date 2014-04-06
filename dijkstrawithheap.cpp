86d7b22962afb2fac7898affc3950e306a3b91a6
ynareshkumar <nareshonline90@gmail.com>
2014-04-05 23:27:51 -0500

    Update dijkstrawithheap.cpp

--- dijkstrawithheap.cpp
+++ dijkstrawithheap.cpp
@@ -2,97 +2,90 @@
 #include <limits.h>
 #include "maxheap.h"
 #include "graphUtility.h"
+#include "mstalgorithms.h"
 
-int s =2,t = 4;
-int dad[NO_OF_VERTICES];
+//int s =2,t = 4
 
-int minimum(int a,int b)
+
+
+class dijkstraimpl: public mstimplementationinterface
 {
-	return a < b ? a:b;
-}
-
-int main()
-{
-	gengraphinterface * randomgraph;
-	int u, v = 0,noofedges = 0;
-	int distu,distv;
+	private:
 	
-	randomgraph = createrandomgraphInstance();
-	randomgraph->generaterandomgraph();		
-	
-	cout<<"\nGraph successfully generated"<<endl;
-	
-	maxheapinterface *heap;
-	heap = createheapInstance();
-	dad[s] = -1;
-	heap->heapinitialize();
-	
-	heap->updatevalue(s,INT_MAX);
-		
-	
-	while(heapsize > 0)
+	int minimum(int a,int b)
 	{
-		u = heap->remove(0);
-		distu = heap->getvalue(u);
-		//cout<<"u is "<<u<<" dist of u "<<distu<<endl;
-		noofedges = 0;
-		v = 0;
-		while(noofedges < VERTEX_DEGREE && v < NO_OF_VERTICES)
+		return a < b ? a:b;
+	}
+	
+	public:
+		dijkstraimpl()
 		{
-			while(adjmatrix[u*NO_OF_VERTICES+v] == 0)
-			{
-				v++;
-			}
-			//cout<<"V selected is "<<v<<endl;
-			if(v < NO_OF_VERTICES)
-			{
-				noofedges++;
-				distv = heap->getvalue(v);
-				//cout<<"Min value chosen is "<<minimum(distu,adjmatrix[u*NO_OF_VERTICES+v])<<endl;
-				if(distv < minimum(distu,adjmatrix[u*NO_OF_VERTICES+v]))
-				{
-					distv = minimum(distu,adjmatrix[u*NO_OF_VERTICES+v]);
-					dad[v] = u;
-					heap->updatevalue(v,distv);
-				}
-				
-				v++;
-			}
-												
-		}
-	}
-	int dst = t;	
-	cout<<"Max bandwidth for vertex "<<t<<" from "<<s<<" is "<<D[t]<<endl;
-	
-	cout<<"The path between "<<dst<<" and "<<s<<" is "<<endl;
-	cout<<dst;
-	while(dst != s)
-	{
-		cout<<"->"<<dad[dst];
-		dst = dad[dst];
-	}
-	cout<<endl;
 			
-	return 0;
-}
-	
-	/*heap->insert(4);		
-	heap->insert(9);
-	heap->insert(6);		
-	heap->insert(45);
-	heap->insert(89);
-	heap->insert(386);
-	heap->insert(20);
+		}
+		
+		void algoimplementation(gengraphinterface * randomgraph,int s,int t)
+		{
+			
+			int u, v = 0,noofedges = 0;
+			int distu,distv;
+			int dad[NO_OF_VERTICES];		
+			
+			maxheapinterface *heap;
+			heap = createheapInstance();
+			dad[s] = -1;
+			heap->heapinitialize();
+			
+			heap->updatevalue(s,INT_MAX);
+				
+			
+			while(heapsize > 0)
+			{
+				u = heap->remove(0);
+				distu = heap->getvalue(u);
+				//cout<<"u is "<<u<<" dist of u "<<distu<<endl;
+				noofedges = 0;
+				v = 0;
+				while(noofedges < VERTEX_DEGREE && v < NO_OF_VERTICES)
+				{
+					while(adjmatrix[u*NO_OF_VERTICES+v] == 0)
+					{
+						v++;
+					}
+					//cout<<"V selected is "<<v<<endl;
+					if(v < NO_OF_VERTICES)
+					{
+						noofedges++;
+						distv = heap->getvalue(v);
+						//cout<<"Min value chosen is "<<minimum(distu,adjmatrix[u*NO_OF_VERTICES+v])<<endl;
+						if(distv < minimum(distu,adjmatrix[u*NO_OF_VERTICES+v]))
+						{
+							distv = minimum(distu,adjmatrix[u*NO_OF_VERTICES+v]);
+							dad[v] = u;
+							heap->updatevalue(v,distv);
+						}
+						
+						v++;
+					}
+														
+				}
+			}
+			int dst = t;	
+			cout<<"Max bandwidth for vertex "<<t<<" from "<<s<<" is "<<D[t]<<endl;
+			
+			/*cout<<"The path between "<<dst<<" and "<<s<<" is "<<endl;
+			cout<<dst;
+			while(dst != s)
+			{
+				cout<<"->"<<dad[dst];
+				dst = dad[dst];
+			}
+			cout<<endl;*/
+		}			
+};
 
-	heap->remove(0);
-	heap->displayheap();
+mstimplementationinterface * createalgo2instance()
+		{
+			return new dijkstraimpl();
+		}
 	
-	heap->updatevalue(0,100);
-	heap->displayheap();
-	heap->updatevalue(1,101);
-	heap->displayheap();
-	heap->updatevalue(1,8);
-	heap->displayheap();
-	heap->updatevalue(0,1);
-	heap->displayheap();*/
 
