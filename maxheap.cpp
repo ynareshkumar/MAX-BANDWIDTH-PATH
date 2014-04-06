226a39ed064e16b1b56a090a08133dc406fd0824
ynareshkumar <nareshonline90@gmail.com>
2014-04-05 23:34:14 -0500

    Update maxheap.cpp

--- maxheap.cpp
+++ maxheap.cpp
@@ -1,107 +1,341 @@
 #include <iostream>
-#include <stack> 
-#include "maxheap.h"
+#include <limits.h>
+#include <stdlib.h>
+#include <vector>
 #include "graphUtility.h"
+#include "maxheap.h"
 
-gengraphinterface * randomgraph;
-maxheapinterface *heap;
-int dad[NO_OF_VERTICES];
-int rank[NO_OF_VERTICES];
+using namespace std;
 
-std::vector<int> maxspanningtree(NO_OF_VERTICES*NO_OF_VERTICES);
+int H[NO_OF_VERTICES];
+int heapsize;
+int edgesize;
+int D[NO_OF_VERTICES];
 
-void makeset(int v)
+std::vector<struct edges> edgeparameters(NO_OF_VERTICES*VERTEX_DEGREE);
+
+class maxheapoperations : public maxheapinterface
 {
-	dad[v] = 0;
-	rank[v] = 0;
-}
-
-int find(int v)
-{
-	std::stack<int> S;
-	int w = v, u;
-	
-	while(dad[w] != 0)
-	{
-		S.push(w);
-		w = dad[w];
-	}
-	
-	while(!S.empty())
-	{
-		u = S.top();
-		S.pop();
-		dad[u] = w;
-	}
-	
-	return w;
-	
-}
-
-void merge(int r1,int r2)
-{
-	if(rank[r1] < rank[r2])
-		dad[r1] = r2;
-	else if(rank[r1] > rank[r2])
-		dad[r2] = r1;
-	else
-	{
-		dad[r2] = r1;
-		rank[r1] += 1;
-	}
-}
-
-void kruskalalgorithm()
-{
-	struct edges e;
-	int r1,r2,u;
-	heap->heapsort();
-	
-	for(int i=0;i<NO_OF_VERTICES;i++)
-		makeset(i);
-	
-	while(edgesize > 0)
-	{
-		e = heap->extractmax();
-		//cout<<" u is "<<e.u<<" v is "<<e.v<<" wt is "<<e.edgeweight<<endl;
-		r1 = find(e.u);
-		r2 = find(e.v);
-		if(r1 != r2)
+	private:
+		
+		
+		int parent(int index)
 		{
-			maxspanningtree[(e.u)*NO_OF_VERTICES+(e.v)] = e.edgeweight;
-			merge(r1,r2);
+			if(index/2+index%2 -1 >= 0)
+				return index/2+index%2 -1;
+			else
+				return -1;
 		}
 		
-	} 
-}
-
-void displaymst(int srcvertex)
-{
-	cout<<"Source vertex "<<srcvertex<<endl;
-	cout<<"Dst vertex Edge weight"<<endl;
-	for(int i=0;i<NO_OF_VERTICES;i++)
-	{
-		if (maxspanningtree[srcvertex*NO_OF_VERTICES+i] != 0)
+		int leftchild(int index)
 		{
-			cout<<i<<"\t    "<<maxspanningtree[srcvertex*NO_OF_VERTICES+i]<<endl;			
+			if(2*index + 1 <= heapsize)
+				return 2*index + 1;
+			else
+				return -1;
 		}
-	}
-}
-
-int main()
-{
-	
-	randomgraph = createrandomgraphInstance();
-	randomgraph->generaterandomgraph();
-	
-	cout<<"\nGraph successfully generated"<<endl;
 		
-	heap = createheapInstance();	
-	kruskalalgorithm();
-	for(int k=0;k < NO_OF_VERTICES ;k++)
-	{
-		displaymst(k);
-	}
+		int rightchild(int index)
+		{
+			if(2*index + 2 <= heapsize)
+				return 2*index + 2;
+			else
+				return -1;
+		}
+		
+		int getmaximum()
+		{
+			return H[0];
+		}	
+		
+		void updateinchild(int i)
+		{
+			int l; 
+			int r,temp; 
+			int max,prev = -1;
+			while(prev != i)
+			{
+				max = i;
+				prev = i;
+				l = leftchild(i);
+				r = rightchild(i);
+				if(l != -1 && D[H[max]] < D[H[l]])		
+				{
+					max = l;
+				}
+				if(r != -1  && D[H[max]] < D[H[r]])
+				{
+					max = r;
+				}
+				if(max != i)
+				{
+					temp = H[max];
+					H[max] = H[i];
+					H[i] = temp;
+					
+					i = max;
+				}
+			}
+		}
+		
+		void updateinparent(int i)
+		{
+			int temp;
+			while(i != -1 && parent(i) != -1 && D[H[parent(i)]] < D[H[i]])
+			{		
+				//cout<<"i is "<<i<<" Parent of i "<<parent(i)<<endl;					
+				temp = H[i];
+				H[i] = H[parent(i)];
+				H[parent(i)] = temp;
+				
+				i = parent(i);
+			}
+		}
+		
+		public:				
+		
+		maxheapoperations()
+		{
+			
+		}
+		
+		void heapinitialize()
+		{
+			heapsize = -1;
+			for(int i=0;i<NO_OF_VERTICES;i++)
+			{
+				H[i] = i;
+				D[i] = INT_MIN;
+				heapsize++;
+			}
+		}		
+				
+		void insert(int val)
+		{
+			int i,temp;
+			heapsize++;
+			D[heapsize] = val;
+			i=heapsize;				
+					
+			updateinparent(i);			
+			
+		}
+		
+		int getvalue(int i)
+		{
+			return D[i];
+		}				
+		
+		
+		int remove(int index)
+		{
+
+			int i=index;	
+			int returnindex = H[index];
+			//D[H[index]] = D[H[heapsize]];
+			H[index] = H[heapsize];		
+			heapsize--;
+			
+			updateinchild(i);			
+			return returnindex;
+			
+		}
+		
+		
+		void displayheap()
+		{
+			cout<<"Printing.."<<endl;
+			for(int i=0;i<=heapsize;i++)
+			{
+				cout<<"Vertex "<<H[i]<<" Edge weight "<<D[H[i]]<<endl;
+			}
+			
+		}
+		
+		void updatevalue(int heapind,int val)
+		{
+			int left,right,heapindex;
+			
+			for(int m=0;m<NO_OF_VERTICES;m++)
+			{
+				if(H[m] == heapind)
+				{
+					heapindex = m;
+					break;
+				}
+			}
+			D[H[heapindex]] = val;			
+			
+			left = leftchild(heapindex);
+			right = rightchild(heapindex);
+			
+			if(parent(heapindex) != -1 && D[H[heapindex]] > D[H[parent(heapindex)]]) 
+			{
+				//cout<<"Updating in parent "<<endl;
+				updateinparent(heapindex);
+			}
+			else if((left != -1 && D[H[heapindex]] < D[H[left]]) || (right != -1 && D[H[heapindex]] < D[H[right]]))
+			{
+				//cout<<"Updating in child "<<endl;
+				updateinchild(heapindex);
+			}
+			//cout<<"Value updated for "<<H[heapindex]<<" with "<<D[1]<<endl;
 	
-	return 0;
-}
+		}
+		
+		void insertedge(struct edges ed)
+		{
+			edgesize++;
+			int i = edgesize;
+			edgeparameters[edgesize] = ed;
+			struct edges par;
+			while(parent(i) != -1) 
+			{
+				par = edgeparameters[parent(i)];
+				if(par.edgeweight < ed.edgeweight)
+				{
+					edgeparameters[parent(i)] = ed;
+					edgeparameters[i] = par;
+				}
+				i = parent(i);
+			}
+			
+		}
+		
+		int leftc(int i)
+		{
+			if(2*i+1 <= edgesize)
+				return 2*i+1;
+			else
+				return -1;
+		}
+		
+		int rightc(int i)
+		{
+			if(2*i+2 <= edgesize)
+				return 2*i+2;
+			else
+				return -1;
+		}
+		
+		
+		struct edges extractmax()
+		{
+			struct edges edg = edgeparameters[0];
+			struct edges temp,maxedge;
+			edgeparameters[0] = edgeparameters[edgesize];
+			edgesize--;
+			int i = 0;
+			int left,right;
+			int max,prev = -1;
+			while(prev != i)
+			{
+				max =i;
+				prev = i;
+				maxedge = edgeparameters[max];
+				left = leftc(i);
+				right = rightc(i);
+				if(left != -1)		
+				{
+					temp = edgeparameters[left];
+					if(temp.edgeweight > maxedge.edgeweight)
+					{
+						max = left;
+						maxedge = edgeparameters[left];
+					}
+				}
+				if(right != -1)
+				{
+					temp = edgeparameters[right];
+					if(temp.edgeweight > maxedge.edgeweight)
+					{
+						max = right;
+						maxedge = edgeparameters[right];
+					}
+				}
+				if(max != i)
+				{
+					temp = edgeparameters[i];
+					edgeparameters[i] = maxedge;
+					if(max == left)
+						edgeparameters[left] = temp;
+					else if(max ==right)
+						edgeparameters[right] = temp;
+					
+					i = max;
+				}
+			}
+			
+			return edg;
+			
+		}
+		
+		void heapsort()
+		{
+			int n = 0, noofedges, v;
+			edgesize = -1;
+			while(n < NO_OF_VERTICES)
+			{				
+				noofedges = 0;
+				v = 0;
+				while(noofedges < VERTEX_DEGREE && v < NO_OF_VERTICES)
+				{
+					while(adjmatrix[n*NO_OF_VERTICES+v] == 0)
+					{
+						v++;
+					}
+					if(v < NO_OF_VERTICES)
+					{
+						noofedges++;
+						struct edges e;
+						e.u = n;
+						e.v = v;
+						e.edgeweight = adjmatrix[n*NO_OF_VERTICES+v];
+						if(n <= v)
+						{
+							//cout<<"Edge weight added is "<<e.edgeweight<<endl;
+							insertedge(e);					
+						}
+						
+					}
+					v++;
+				}
+				n++;
+				
+			}
+		}
+		
+	};
+	
+	maxheapinterface * createheapInstance()
+		{
+			return new maxheapoperations();
+		}
+
+	
+	/*int main()
+	{
+		initialize();
+		insert(4);		
+		insert(9);
+		insert(6);		
+		insert(45);
+		insert(89);
+		insert(386);		insert(20);
+		
+		//display();
+		remove(0);
+		display();
+		updatevalue(0,100);
+		display();
+		updatevalue(1,101);
+		display();
+		updatevalue(1,8);
+		display();
+		updatevalue(0,1);
+		display();
+		//cout<<"Parent "<<D[leftchild(1)]<<endl;
+		return 0;
+	}*/
+	
+
